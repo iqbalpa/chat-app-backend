fd6a39d7cd12aa9364b9267b87e82794fc67cda2
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-23 06:27:54 +0700

    create socket

--- src/websockets/chat.gateway.ts
+++ src/websockets/chat.gateway.ts
@@ -1,11 +1,19 @@
-import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import {
+  OnModuleInit,
+  UseFilters,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { WebsocketExceptionFilter } from './ws-exception.filter';
+import { Server, Socket } from 'socket.io';
 
 class ChatMessage {
   @IsNotEmpty()
@@ -17,12 +25,29 @@ class ChatMessage {
   message: string;
 }
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: { origin: '*' },
+})
 @UseFilters(new WebsocketExceptionFilter())
-export class ChatGateway {
-  @SubscribeMessage('chat')
+export class ChatGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(`connected to socket: ${socket.id}`);
+    });
+  }
+
+  @SubscribeMessage('sendMessage')
   @UsePipes(new ValidationPipe())
-  handleMessage(@MessageBody() message: ChatMessage) {
-    console.log(message);
+  handleMessage(
+    @MessageBody() message: ChatMessage,
+    @ConnectedSocket() _client: Socket,
+  ) {
+    this.server.emit('receiveMessage', {
+      ...message,
+      time: new Date().toString(),
+    });
   }
 }
