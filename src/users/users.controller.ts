642f2fd848b418277d4c392178eb6ea3e61a2dd3
iqbalpa <iqbalpahlevi16@gmail.com>
2024-06-26 06:34:07 +0700

    feat(friends): add auth guard for get user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -11,6 +12,7 @@ export class UsersController {
     return this.usersService.getAllIds();
   }
 
+  @UseGuards(AuthGuard)
   @Get(":id")
   async getById(@Param('id') id: string): Promise<User | null> {
     return this.usersService.getById(id)
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   providers: [UsersService],
-  exports: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
